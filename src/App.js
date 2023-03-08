1544d852dc3b40983461cb84f242e240c3cf03f0
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-08 19:48:08 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./Styles/App.css";
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
