4ac9fe3a9ef1dafe8e016c92ddc553beebe37cb4
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-09 20:07:48 +0100

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
+  const title = "Neki naslov";
+
   return (
     <div className="App">
-      <h1>First Typescript</h1>
+      <Header title={title}/>
     </div>
   );
 }
