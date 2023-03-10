64c695b8bd2ec1913ea58b5094f9ef81864428b2
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-10 19:56:54 +0100

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Counter from './Components/Counter';
 import Header from './Components/Header';
@@ -6,12 +6,13 @@ import Section from './Components/Section';
 
 function App() {
   const title = "Neki naslov";
+  const [count, setCount] = useState<number>(1);
 
   return (
     <div className="App">
       <Header title={title}/>
       <Section title="Drugi naslov">This is my section</Section>
-      <Counter />
+      <Counter setCount={setCount}>Count is {count}<Counter/>
     </div>
   );
 }
